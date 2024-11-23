2a3e3b6023734e88de272f0da59c099243b5f230
Mihir123456JJ <kalothiamihir@gmail.com>
2024-11-23 18:57:25 +0530

    All about Countries

---
+++ main.py
@@ -0,0 +1,37 @@
+class Mexico():
+    def capital(self):
+        print("Mexico city is the capital of Mexico")
+    def language(self):
+        print("Mexican is spoken in Mexico")
+    def devlop(self):
+        print("Most parts of Mexico our devloped but some our underdevloped")
+class Africa():
+    def capital(self):
+        print("Ethopia is the capital of Africa")
+    def language(self):
+        print("Africans is spoken in Africa")
+    def devlop(self):
+        print("Africa is still in the process of devloping")
+class Spain():
+    def capital(self):
+        print("Madrid  is the capital of Spain")
+    def language(self):
+        print("Spanish is spoken in Spain")
+    def devlop(self):
+        print("Spain is a devloped country")
+class Canada():
+    def capital(self):
+        print("Ottawa  is the capital of Canada")
+    def language(self):
+        print("Canadian is spoken in Canada")
+    def devlop(self):
+        print("Canada is a devloped country")
+y=Mexico()
+X=Africa()
+Z=Spain()
+K=Canada()
+for i in(X,y,Z,K):
+    i.capital()
+    i.language()
+    i.devlop() 
+
